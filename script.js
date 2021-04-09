d286f62b09237cc3012f80fa140f7148e9479b7a
Stephane Baroux <stephane.baroux@gmail.com>
2021-04-10 00:28:37 +0200

    mobile layout done and working
    Mobile layout all done and working.

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
 const buttonsArray = Array.from(document.getElementsByClassName('clickable'));
 const answersArray = Array.from(document.getElementsByClassName('answer'));
-const buttonsVisibilityValuesArray = [true,true,true,true,true];
+const arrowsArray = Array.from(document.getElementsByClassName('arrow'));
+const questionsArray = Array.from(document.getElementsByClassName("question"))
+const buttonsVisibilityValuesArray = [false,false,false,false,false];
 
 function toggleVisibility(item){
-    let itemVisibility = buttonsVisibilityValuesArray[item];
-    let answer = answersArray[item];
+    const itemVisibility = buttonsVisibilityValuesArray[item];
+    const answer = answersArray[item];
+    const arrow = arrowsArray[item];
+    const question = questionsArray[item];
+
+    
+    closeAll();
 
     if(itemVisibility){
         answer.style.display = "none";
+        question.style.fontWeight = "400";
+        question.style.fontSize = "1em";
+        arrow.style.rotate ="0deg";
         buttonsVisibilityValuesArray[item] = false;
         return;
     } else {
         answer.style.display = "block";
+        question.style.fontWeight = "700";
+        question.style.fontSize = "0.9em";
+        arrow.style.rotate ="180deg";
         buttonsVisibilityValuesArray[item] = true;
         return;
     }
 };
 
+function resetMenuFonts(){
+    
+}
+
+function closeAll(){
+    for (var item=0 ; item < buttonsArray.length; item++ ) { 
+        const answer = answersArray[item];
+        const arrow = arrowsArray[item];
+        const question = questionsArray[item];
+
+        buttonsVisibilityValuesArray[item] = false;
+        answer.style.display = "none";
+        question.style.fontWeight = "400";
+        question.style.fontSize = "1em";
+        arrow.style.rotate ="0deg";
+    }
+}
+
+
+closeAll();
+
 for (var item=0 ; item < buttonsArray.length; item++ ) { 
+    buttonsVisibilityValuesArray[item] = false;
     let thisElement = item;
     buttonsArray[item].addEventListener("click", () => toggleVisibility(thisElement));
 }
